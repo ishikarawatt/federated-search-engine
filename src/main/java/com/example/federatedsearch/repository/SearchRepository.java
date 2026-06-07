81e33c0e4bd147faa166da2c021c2448f080e2f2
Ishika Rawat <ishikarwt123@gmail.com>
2026-06-07 22:16:43 +0530

    Update SearchRepository.java

--- src/main/java/com/example/federatedsearch/repository/SearchRepository.java
+++ src/main/java/com/example/federatedsearch/repository/SearchRepository.java
@@ -15,7 +15,6 @@ public class SearchRepository {
     // Popular queries tracker
     private final Map<String, Integer> popularQueries = new ConcurrentHashMap<>();
    
-
     /**
      * Saves a search query to history
      */
